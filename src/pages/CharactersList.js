6c8588a405c479b877c829c6238b89176c20bec4
Laura <65158950+lauratro@users.noreply.github.com>
2021-10-04 22:00:39 +0200

    CharactersList and useCharacters component created

---
+++ src/pages/CharactersList.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { useCharacters } from "../useCharacters";
+import "./CharacterList.css";
+
+export default function CharactersList() {
+  const { error, loading, data } = useCharacters();
+  console.log({ error, loading, data });
+  if (loading) {
+    return <div>Spinner...</div>;
+  }
+  if (error) {
+    return <div>There is an error</div>;
+  }
+  return (
+    <div className="CharacterList">
+      {data.characters.results.map((character) => {
+        return (
+          <div key={character.id}>
+            <img src={character.image} />
+            <h2>{character.name}</h2>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
